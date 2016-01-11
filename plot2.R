7c8068dd48de047865a611ece77a170407548347
xuxiao0330 <xuxia0330@hotmail.com>
2016-01-11 22:55:04 +0800

    Assignment 1 Edit

--- plot2.R
+++ plot2.R
@@ -1,16 +1,5 @@
-#setting working directory 
-setwd("~/datafiles")
-#reading table
-dt <- read.table("household_power_consumption.txt",sep=";",header = TRUE,stringsAsFactors=FALSE)
-#making date format
-dt$Date <- as.Date(dt$Date,format="%d/%m/%Y")
-#subsetting 2007-02-01 ~ 2007-02-02
-dt1 <- subset(dt,dt$Date == "2007-02-01"|dt$Date =="2007-02-02")
-#Setting time in dt1
-dt1$Time <- strptime(paste(dt1$Date,dt1$Time),format = "%Y-%m-%d %H:%M:%S")
-#Setting numeric of Global Active Power
-dt1$Global_active_power<- as.numeric(dt1$Global_active_power)
 #Plot 2
 png("plot2.png",height = 480, width = 480)
 plot(dt1$Time,dt1$Global_active_power,type = "l",xlab = "",ylab = "Global Active Power (kilowatts)")
 dev.off()
+
--- plot4.R
+++ plot4.R
@@ -1,15 +1,3 @@
-#setting working directory 
-setwd("~/datafiles")
-#reading table
-dt <- read.table("household_power_consumption.txt",sep=";",header = TRUE,stringsAsFactors=FALSE)
-#making date format
-dt$Date <- as.Date(dt$Date,format="%d/%m/%Y")
-#subsetting 2007-02-01 ~ 2007-02-02
-dt1 <- subset(dt,dt$Date == "2007-02-01"|dt$Date =="2007-02-02")
-#Setting time in dt1
-dt1$Time <- strptime(paste(dt1$Date,dt1$Time),format = "%Y-%m-%d %H:%M:%S")
-#Setting numeric of Global Active Power
-dt1$Global_active_power<- as.numeric(dt1$Global_active_power)
 #plot 4
 png("plot4.png")
 # setting the plot
